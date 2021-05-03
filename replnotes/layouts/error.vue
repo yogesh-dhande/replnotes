aa0db6703073b6fb5a62e743ef4cc31d8df146b0
Yogesh Dhande <yogeshkd@gmail.com>
2021-05-02 19:04:48 -0700

    Added some minimal styling to the custom error page

--- replnotes/layouts/error.vue
+++ replnotes/layouts/error.vue
@@ -1,14 +1,40 @@
 <template>
-  <div class="container">
-    <h1 v-if="error.statusCode === 404">Page not found</h1>
-    <h1 v-else>An error occurred</h1>
-    <NuxtLink to="/">Home page</NuxtLink>
-  </div>
+  <form-page>
+    <div class="rounded-md bg-gray-800 p-4 my-auto text-indigo-200">
+      <div class="flex flex-col items-center">
+        <div class="flex-shrink-0 h-64 w-64 opacity-75">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+        </div>
+        <div class="ml-3 text-4xl font-extrabold">
+          <h1 v-if="error.statusCode === 404">Page not found</h1>
+          <h1 v-else-if="error.message">{{ error.message }}</h1>
+          <h1 v-else>An error occurred.</h1>
+        </div>
+      </div>
+    </div>
+  </form-page>
 </template>
 
 <script>
+import FormPage from "@/components/FormPage";
+
 export default {
-  props: ['error'],
-  layout: 'default' // you can set a custom layout for the error page
-}
+  props: ["error"],
+  components: {
+    FormPage,
+  },
+  layout: "default", // you can set a custom layout for the error page
+};
 </script>
