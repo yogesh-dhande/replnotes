7e0bd52369ebe84f0b19b222fed6d4ca18aa0f66
Yogesh Dhande <yogeshkd@gmail.com>
2021-05-08 17:11:33 -0700

    Fixed a bug in Modal.vue that prevented it from showing up in some screen sizes

--- replnotes/components/Modal.vue
+++ replnotes/components/Modal.vue
@@ -1,13 +1,13 @@
 <template>
+  <div
+    v-if="visible"
+    class="fixed z-10 inset-0 overflow-y-auto text-gray-300"
+    @click="$emit('close')"
+  >
     <div
-        v-if="visible"
-        class="fixed z-10 inset-0 overflow-y-auto text-gray-300"
-        @click="$emit('close')"
+      class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center block sm:p-0"
     >
-        <div
-            class="flex items-end justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0"
-        >
-            <!--
+      <!--
       Background overlay, show/hide based on modal state.
 
       Entering: "ease-out duration-300"
@@ -17,17 +17,17 @@
         From: "opacity-100"
         To: "opacity-0"
     -->
-            <div class="fixed inset-0 transition-opacity" aria-hidden="true">
-                <div class="absolute inset-0 bg-gray-100 opacity-50"></div>
-            </div>
+      <div class="fixed inset-0 transition-opacity" aria-hidden="true">
+        <div class="absolute inset-0 bg-gray-100 opacity-50"></div>
+      </div>
 
-            <!-- This element is to trick the browser into centering the modal contents. -->
-            <span
-                class="hidden sm:inline-block sm:align-middle sm:h-screen"
-                aria-hidden="true"
-                >&#8203;</span
-            >
-            <!--
+      <!-- This element is to trick the browser into centering the modal contents. -->
+      <span
+        class="hidden sm:inline-block sm:align-middle sm:h-screen"
+        aria-hidden="true"
+        >&#8203;</span
+      >
+      <!--
       Modal panel, show/hide based on modal state.
 
       Entering: "ease-out duration-300"
@@ -37,31 +37,31 @@
         From: "opacity-100 translate-y-0 sm:scale-100"
         To: "opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
     -->
-            <slot></slot>
-        </div>
+      <slot></slot>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'modal',
-    props: {
-        show: {
-            type: Boolean,
-            default: false,
-        },
+  name: "modal",
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
     },
-    data() {
-        return {
-            visible: this.show,
-        }
+  },
+  data() {
+    return {
+      visible: this.show,
+    };
+  },
+  watch: {
+    show(newvalue) {
+      this.visible = newvalue;
     },
-    watch: {
-        show(newvalue) {
-            this.visible = newvalue
-        },
-    },
-}
+  },
+};
 </script>
 
 <style>
