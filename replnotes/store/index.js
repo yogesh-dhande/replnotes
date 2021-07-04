cc38e52bd18c80918d8cf594ab078ecf4051ce5f
Yogesh Dhande <yogeshkd@gmail.com>
2021-07-03 17:47:41 -0700

    Fixed Caddyfile

--- replnotes/store/index.js
+++ replnotes/store/index.js
@@ -1,100 +1,98 @@
 export const state = () => ({
-    authUserId: null,
-    showFeedbackModal: false,
-    token: null,
-    currentUser: {},
-    isEmailVerified: null,
-    readonly: {},
-    error: {
-      response: {
-        status: "404",
-        data: {
-          message: "Page Not Found",
-        },
-      },
-    },
-  })
-
-export const getters = {
-    userPostUrls(state) {
-      if (!state.currentUser.posts) {
-        return [];
+  authUserId: null,
+  showFeedbackModal: false,
+  token: null,
+  currentUser: {},
+  isEmailVerified: null,
+  readonly: {},
+  error: {
+    response: {
+      status: "404",
+      data: {
+        message: "Page Not Found"
       }
-      return Object.values(state.currentUser.posts).map((post) => {
-        return post.url;
-      });
-    },
-    userTags(state) {
-      if (!state.currentUser.posts) {
-        return [];
-      }
-      let tagList = Object.values(state.currentUser.posts).map((post) => {
-        return post.tags;
-      });
-
-      return [...new Set([].concat.apply([], tagList))];
-    },
-    loggedIn(state) {
-      return state.authUserId
     }
   }
+});
+
+export const getters = {
+  userPostUrls(state) {
+    if (!state.currentUser.posts) {
+      return [];
+    }
+    return Object.values(state.currentUser.posts).map(post => {
+      return post.url;
+    });
+  },
+  userTags(state) {
+    if (!state.currentUser.posts) {
+      return [];
+    }
+    let tagList = Object.values(state.currentUser.posts).map(post => {
+      return post.tags;
+    });
+
+    return [...new Set([].concat.apply([], tagList))];
+  },
+  loggedIn(state) {
+    return state.authUserId;
+  }
+};
 
 export const actions = {
-  async nuxtServerInit({ commit }, { res }) {
+  async nuxtServerInit({ commit }, { res, req }) {
+    if (process.server) {
+      console.log(req.headers);
+    }
     if (res && res.locals && res.locals.user) {
-      const { allClaims: claims, idToken: token, ...authUser } = res.locals.user
+      const {
+        allClaims: claims,
+        idToken: token,
+        ...authUser
+      } = res.locals.user;
       commit("SET_AUTH_STATE", authUser);
-
     }
   },
 
   async onAuthStateChangedAction({ commit }, { authUser }) {
-    commit("SET_AUTH_STATE", authUser? authUser: {});
+    commit("SET_AUTH_STATE", authUser ? authUser : {});
 
     if (!authUser) {
       // remove state
-      commit('SET_USER', {})
+      commit("SET_USER", {});
       commit("SET_TOKEN", null);
-
-
     } else {
-      authUser.getIdToken(/* forceRefresh */ true).then((token) => {
+      authUser.getIdToken(/* forceRefresh */ true).then(token => {
         this.$fire.analytics.setUserId(authUser.uid);
         commit("SET_TOKEN", token);
         commit("SET_EMAIL_VERIFIED", authUser.emailVerified);
 
-        this.$usersCollection.doc(authUser.uid).onSnapshot((snap) => {
+        this.$usersCollection.doc(authUser.uid).onSnapshot(snap => {
           commit("SET_USER", snap.data() || {});
-        })
-  
-        this.$readonlyCollection
-          .doc(authUser.uid)
-          .onSnapshot((snap) => {
-            commit("SET_READONLY_DATA", snap.data() || {});
-          })
-         
-      })
+        });
 
+        this.$readonlyCollection.doc(authUser.uid).onSnapshot(snap => {
+          commit("SET_READONLY_DATA", snap.data() || {});
+        });
+      });
     }
-  },
-
   }
+};
 
 export const mutations = {
-    SET_USER(state, val) {
-      state.currentUser = val;
-    },
-    SET_AUTH_STATE(state, authUser) {
-      state.authUserId = authUser.uid
-    },
-    SET_TOKEN(state, val) {
-      state.token = val;
-    },
-    SET_EMAIL_VERIFIED(state, val) {
-      state.isEmailVerified = val;
-    },
-    SET_READONLY_DATA(state, data) {
-      state.readonly = data;
-    },
-
+  SET_USER(state, val) {
+    state.currentUser = val;
+  },
+  SET_AUTH_STATE(state, authUser) {
+    state.authUserId = authUser.uid;
+  },
+  SET_TOKEN(state, val) {
+    state.token = val;
+  },
+  SET_EMAIL_VERIFIED(state, val) {
+    state.isEmailVerified = val;
+  },
+  SET_READONLY_DATA(state, data) {
+    state.readonly = data;
   }
+};
