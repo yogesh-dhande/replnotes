8d70205e57f3222fc5c1a3e2eaecb0e450079bad
Yogesh Dhande <yogeshkd@gmail.com>
2021-07-08 07:28:51 -0700

    Created user and admin folders

---
+++ replnotes/pages/admin/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <div>
+      <landing />
+      <!-- <pricing /> -->
+    </div>
+  </div>
+</template>
+
+<script>
+import Landing from "@/partials/Landing";
+
+export default {
+  name: "home",
+  components: {
+    UserPosts,
+    Landing,
+  },
+
+  layout: "default",
+};
+</script>
+
+<style>
+</style>
